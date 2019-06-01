35d35cfcd92b97929b084172e682a090dbe9b64d
unknown <yogesh.jadhav@gmail.com>
2019-06-01 10:24:25 +0530

    Initial code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-environment';
+
+  apiURL = '';
+
+  ngOnInit() {
+    this.apiURL = environment.API_URL;
+  }
 }
